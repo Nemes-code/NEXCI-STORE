dfa9acadcbbcd454ae802a85f6c9ae2fca4d2c27
Nemes <sangawenemes@gmail.com>
2023-11-27 16:15:48 +0300

    edits

--- src/home.js
+++ src/home.js
@@ -3,19 +3,22 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <navigation />
-
       <div className="nav">
         <h1 className="head">NEXCI STORE</h1>
-        <img className="image" src="download.jpg" alt="Imageyapili" />
-        <h3>
-          An ERP for
-          <p>---shops</p>
-          <p>---supermarkets</p>
-          <p>---mini shops</p>
-          <p>---Petrol stations</p>
-          <p>---and any selling business</p>
-        </h3>
+        <div class="grid-container">
+          <div class="item">
+            <img className="image" src="hr.jpg" alt="Ijdmage" />
+          </div>
+          <div class="item">
+            <img className="image" src="hr.jpg" alt="Ijdmage" />
+          </div>
+          <div class="item">
+            <img className="image" src="hr.jpg" alt="Ijdmage" />
+          </div>
+          <div class="item">
+            <img className="image" src="hr.jpg" alt="Ijdmage" />
+          </div>
+        </div>
       </div>
     </div>
   );
