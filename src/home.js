5d206da1b7837654e8d074e6cbf00f36b271ddfe
Nemes <sangawenemes@gmail.com>
2023-11-29 12:58:58 +0300

    edited

--- src/home.js
+++ src/home.js
@@ -5,18 +5,18 @@ function Home() {
     <div>
       <div className="nav">
         <h1 className="head">NEXCI STORE</h1>
-        <div class="grid-container">
+        <div className="grid-container">
           <div class="item">
-            <img className="image" src="hr.jpg" alt="Ijdmage" />
+            <img className="images" src="hr.jpg" alt="Ijdmage" />
           </div>
           <div class="item">
-            <img className="image" src="hr.jpg" alt="Ijdmage" />
+            <img className="images" src="inventory.jpg" alt="Ijdmage" />
           </div>
           <div class="item">
-            <img className="image" src="hr.jpg" alt="Ijdmage" />
+            <img className="images" src="pos.png" alt="Ijdmage" />
           </div>
           <div class="item">
-            <img className="image" src="hr.jpg" alt="Ijdmage" />
+            <img className="images" src="crm.jpg" alt="Ijdmage" />
           </div>
         </div>
       </div>
